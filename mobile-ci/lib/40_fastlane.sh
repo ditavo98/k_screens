bac04b58bf310fc5d7f3b6c0620a2d6ddc628b22
Dita <146700365+ditavo98@users.noreply.github.com>
2026-05-25 11:50:16 +0700

    Update 40_fastlane.sh

--- mobile-ci/lib/40_fastlane.sh
+++ mobile-ci/lib/40_fastlane.sh
@@ -22,7 +22,8 @@ _fl_create_gemfile() {
 source "https://rubygems.org"
 
 gem "fastlane", ">= 2.220.0"
-gem "jwt",      ">= 2.7.0"    # JWT ES256 cho ASC API
+gem "jwt",      ">= 2.7.0"
+gem "json",     ">= 2.7.2"
 
 plugins_path = File.join(File.dirname(__FILE__), "fastlane", "Pluginfile")
 eval_gemfile(plugins_path) if File.exist?(plugins_path)
